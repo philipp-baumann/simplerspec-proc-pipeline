a45a37a733954e29be76d93f2d1ecb5f58c4c669
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-04-04 00:47:15 +0200

    Improve documentation and adapt notation ("x-axis" instead of imprecise "x-unit")

--- R/resample-spc-column-in.R
+++ R/resample-spc-column-in.R
@@ -1,52 +1,72 @@
-#' @title Resample spectra to new X-unit interval
-#' @description Resamples or interpolates spectra types stored in tibble column
-#' after gathering spectra by `simplerspec::gather_spc()`.
-#' @param spc_tbl Spectra data in a tibble object. The spectra tibble needs at
-#' least the presence of one of the the columns `spc`, `spc`,
-#' (list-colums with spectral `data.table`s stored
-#' in a list) and `wavenumbers` or `wavelengths` (list-column that
-#' contains list of X-axis values corresponding to each spectrum in `spc`
-#' in wavenumber or wavelength numeric values).
-#' @param column_in Character vector or name of length 1L specifying the
-#' name of list-column that contains the spectra to be resampled.
-#' @param x_unit Character vector or name of length 1L specifying the
-#' list-column that contains the list of X-axis unit values of corresponding
-#' spectra in `spc_tbl`. Possible values are `"wavenumber"` (default)
+#' @title Resample spectra to new x-axis interval
+#' @description Resamples (interpolates) different spectra types with
+#' corresponding x-axis values that are both stored in list-columns of a spectra
+#' tibble. A spectra tibble hosts spectra, X-unit vectors, metadata, and
+#' further linked data with standardized naming conventions. Data input for
+#' resampling can for example be generated with `simplerspec::gather_spc()`.
+#' Resampling is a key harmonizing step to process and later model spectra
+#' measured at different resolution and spectral ranges (i.e., different
+#' spectrometer devices or measurement settings).
+#' @param spc_tbl Spectra data in a tibble object (classes
+#' `"tbl_df", "tbl", "data.frame"`). The spectra tibble needs to contain at
+#' least of one of the the spectra columns `spc`, `spc_rs`, `spc_mean`,
+#' `spc_nocomp`, `sc_sm`, `sc_rf`, or `spc_pre` (list-colums with spectral
+#' `data.table`s), and `wavenumbers` or `wavelengths` (list-column with vectors
+#' of x-axis values corresponding to each spectrum). The section *"Matching
+#' spectrum type and corresponding x-axis type"* describes the spectra types
+#' and corresponding x-axis types.
+#' @param column_in Character vector of length 1L or symbol/name
+#' specifying the name of list-column that contains the spectra to be resampled.
+#' @param x_unit Character vector of length 1L specifying the measurement unit
+#' of the x-axis values (list-column) of the input spectra in `spc_tbl`.
+#' Possible values are `"wavenumber"` (default)
 #' or `"wavelength"`. Wavenumber is a convenient measure of frequency in the
-#' mid-infrared spectral range, where wavelength often used for the visible and
-#' near-infrared range.
-#' @param wn_lower Numeric value (integer or float) of lowest wavenumber.
-#' This argument will only be used if `x_unit = "wavenumber"`. The value
-#' serves as starting value when creating the new wavenumber sequence
-#' that the spectra will be resampled upon. Default value is 500 (i.e.,
-#' in reciprocal centimeters).
-#' @param wn_upper Numeric value (integer or float) of highest wavenumber.
-#' This argument will only be used if `x_unit = "wavenumber`}. The value
-#' will be used as last value when creating the new wavenumber sequence
-#' that the spectra will be resampled upon. Default value is 4000 (i.e.,
-#' in reciprocal centimeters).
-#' @param wn_interval Numeric value value (integer or float) of the wavenumber
-#' increment when creating the new wavenumber sequence that the spectra will be
-#' resampled upon. Default value is 2 (i.e., in reciprocal centimeters).
-#' @param wl_lower Numeric value (integer or float) of lowest wavelength.
-#' This argument will only be used if `x_unit = "wavelength"`. The value
-#' serves as starting value when creating the new wavenumber sequence
-#' that the spectra will be resampled upon. Default value is 350 (i.e. in
-#' nanometers).
-#' @param wl_upper Numeric value (integer or float) of highest wavelength.
-#' This argument will only be used if `x_unit = "wavelength"`. The value
-#' will be used as last value when creating the new wavenumber sequence
-#' that the spectra will be resampled upon. Default value is 2500 (i.e., in
-#' nanometers).
-#' @param wl_interval Numeric value (integer or float) of the wavelength
-#' increment when creating the new wavenumber sequence that the spectra will be
+#' mid-infrared spectral range, where wavelength is  often used for the visible
+#' and near-infrared range.
+#' @param wn_lower Numeric value of lowest wavenumber. This argument will only
+#' be used if `x_unit = "wavenumber"`. The value serves as starting value for
+#' the new wavenumber sequence that the spectra will be resampled upon. Default
+#' value is 500 (i.e., in reciprocal centimeters).
+#' @param wn_upper Numeric value of highest wavenumber. This argument will only
+#' be used if `x_unit = "wavenumber`. The value will be used as last value of
+#' the new wavenumber sequence that the spectra will be resampled upon. Default
+#' value is 4000 (i.e., in reciprocal centimeters).
+#' @param wn_interval Numeric value of the wavenumber increment for the new
+#' wavenumber sequence that the spectra will be resampled upon. Default value
+#' is 2 (i.e., in reciprocal centimeters).
+#' @param wl_lower Numeric value of lowest wavelength. This argument will only
+#' be used if `x_unit = "wavelength"`. The value serves as starting value of
+#' the new wavenumber sequence that the spectra will be resampled upon.
+#' Default value is 350 (i.e. in nanometers).
+#' @param wl_upper Numeric value of highest wavelength. This argument will only
+#' be used if `x_unit = "wavelength"`. The value will be used as last value of
+#' the new wavenumber sequence that the spectra will be resampled upon. Default
+#' value is 2500 (i.e., in nanometers).
+#' @param wl_interval Numeric value of the wavelength
+#' increment of the new wavenumber sequence that the spectra will be
 #' resampled upon. This argument will only be used if `x_unit = "wavelength"`.
 #' Default value is 1 (i.e., in nanometers).
-#' @return A spectra tibble containing the following additional columns
-#' added to the input spectra tibble (`spc_tbl`): `wavenumbers_rs` or
-#' `wavelengths_rs` as list-columns containing the resampled wavenumbers
-#' or wavelengths, `spc_rs` list-column of resampled spectra returned
-#' as list of data.tables (class "data.table" and "data.frame").
+#' @param interpol_method
+#' @return A spectra tibble (`spc_tbl`) containing two added list-columns:
+#' * `spc_rs:` Resampled spectra as list of `data.table`s
+#' * `wavenumbers_rs` or `wavelengths_rs`: Resampled x-axis values as list of
+#' numeric vectors
+#' @section Matching spectrum type and corresponding x-axis type:
+#' The combinations of input spectrum types (`column_in`) and
+#' corresponding x-axis types are generated from a simple lookup list. The
+#' following key-value pairs can be matched at given key, which is the column
+#' name from `column_in` containing the spectra:
+#' and `x_unit`:
+#' * `"spc"` : `"wavenumbers"` or `"wavelengths"` (raw spectra)
+#' * `"spc_rs"` : `"wavenumbers_rs"` or `"wavelengths_rs"`) (resampled spectra)
+#' * `"spc_mean"` : `"wavenumbers_rs"` or `"wavelengths_rs"` (mean spectra)
+#' * `"spc_nocomp"` `"wavenumbers"` or `"wavelengths"` (spectra prior atmopheric
+#'   compensation)
+#' * `"sc_sm" : c("wavenumbers_sc_sm", "wavelengths_sc_sm")` (single channel
+#'   sample spectra)
+#' * `"sc_rf" : c("wavenumbers_sc_rf", "wavelengths_sc_rf")` (single channel
+#'   reference spectra)
+#' * `"spc_pre" : "xvalues_pre"` (preprocessed spectra)
 #' @export
 resample_spc <- function(spc_tbl,
                          column_in = "spc",
@@ -66,7 +86,7 @@ resample_spc <- function(spc_tbl,
   )
 
   # Lookup list to match spectrum types and corresponding X-unit types
-  spc_xunit_types <- list(
+  spc_xaxis_types <- list(
     "spc" = c("wavenumbers", "wavelengths"), # raw/unprocessed
     "spc_rs" = c("wavenumbers_rs", "wavelengths_rs"), # resampled
     "spc_mean" = c("wavenumbers_rs", "wavelengths_rs"), # mean
@@ -75,7 +95,7 @@ resample_spc <- function(spc_tbl,
     "sc_rf" = c("wavenumbers_sc_rf", "wavelengths_sc_rf"), # single channel reference
     "spc_pre" = rep("xvalues_pre", 2) # preprocessed
   )
-  spctypes <- names(spc_xunit_types)
+  spctypes <- names(spc_xaxis_types)
   column_spc <- match.arg(column_in_chr, spctypes)
 
   x_unit <- match.arg(x_unit)
@@ -86,7 +106,7 @@ resample_spc <- function(spc_tbl,
   interpol_method <- match.arg(interpol_method)
 
   # Final selection of `x_unit` column name string from user input and lookup
-  x_unit_sel <- spc_xunit_types[[column_spc]][x_unit_int]
+  x_unit_sel <- spc_xaxis_types[[column_spc]][x_unit_int]
 
   # Both columns with X-values and input spectra need to be present in `spc_tbl`
   colnm <- colnames(spc_tbl)
@@ -95,7 +115,7 @@ resample_spc <- function(spc_tbl,
   # Extract list-column containing spectra
   spc_in_list <- dplyr::pull(spc_tbl, !!column_in)
 
-  # Extract list-column containing X-unit values
+  # Extract list-column containing X-axis values
   xvalues_in_list <- dplyr::pull(spc_tbl, !!x_unit_sel)
 
   # Automatically check the arrangement of the input x-Unit values;
@@ -118,7 +138,7 @@ resample_spc <- function(spc_tbl,
   }
   xvalue_order <- xvalue_order_chr[1L]
 
-  # Generate sequence of new X-unit values
+  # Generate sequence of new x-axis values
   switch(x_unit_int,
          `1L` = {
            xvalues_out <- seq(from = wn_lower, to = wn_upper, by = wn_interval)
@@ -131,7 +151,7 @@ resample_spc <- function(spc_tbl,
 
   if (xvalue_order == "descending") xvalues_out <- rev(xvalues_out)
 
-  # Repeat sequence of new (resampled) X-unit values in list (for every obs.)
+  # Repeat sequence of new (resampled) x-axis values in list (for every obs.)
   xvalues_out_list <- rep(list(xvalues_out), nrow(spc_tbl))
   names(xvalues_out_list) <- names(spc_in_list)
 
